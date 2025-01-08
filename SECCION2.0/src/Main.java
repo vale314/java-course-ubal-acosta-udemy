6339c02ba1a7d1cf077f8dbca8ae787f4ed1b940
electrovale314@hotmail.com <electrovale314@hotmail.com>
2025-01-08 13:18:23 -0600

    Terminamos seccion 2

---
+++ SECCION2.0/src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        String name = "John Doe";
+        double precioProducto = 100.5;
+        int cantidad = 15;
+        boolean disponible = true;
+        var apellido = "Nullo";
+        final var DIAS_SEMANA = 7;
+
+        System.out.println("Nombre: " + name);
+        System.out.println("Precio: " + precioProducto);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("Disponible: " + disponible);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Dias de la semana: " + DIAS_SEMANA);
+    }
+}
